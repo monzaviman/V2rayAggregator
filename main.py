ece9eb2eb9db6e8ace27f04e6f8cf379db051656
Dark6273 <mahdi.khosravi6273@gmail.com>
2023-05-14 15:38:18 +0330

    update code and add another server links

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import requests
 import base64
 import sqlite3
-import telegram
-from time import  sleep
-from telegram.constants import ParseMode
-import asyncio
-
+from time import sleep
 import tracemalloc
+
 tracemalloc.start()
 
-def base64_to_string(inp:str):
+
+def base64_to_string(inp: str):
     return base64.b64decode(inp).decode('UTF-8')
 
 
-def server_list_generator(servers:str):
-
+def server_list_generator(servers: str):
     server_list = []
 
     for server in servers.split('\n'):
@@ -22,30 +19,28 @@ def server_list_generator(servers:str):
 
     return server_list
 
+
 def insert_to_db(server):
     c = conn.cursor()
-    c.execute("INSERT INTO servers (server) VALUES (?)",(server,))
+    c.execute("INSERT INTO servers (server) VALUES (?)", (server,))
     c.close()
     pass
 
 
 def check_exist_in_db(server):
     c = conn.cursor()
-    exists = c.execute("SELECT COUNT(*) FROM servers WHERE server=?",(server,)).fetchone()[0]
-    if int(exists) == 1:
+    exists = c.execute("SELECT COUNT(*) FROM servers WHERE server=?", (server,)).fetchone()[0]
 
+    if int(exists) == 1:
         c.close()
         return True
 
-
     elif int(exists) == 0:
-
         c.close()
         return False
 
 
-
-def send_to_telegram(server_link:str):
+def send_to_telegram(server_link: str):
     bot_token = '5260808018:AAEcUiHOnA3z2A2PeVp-9KrDFaTNaQ5hTWU'
     chat_id = '-1001643529458'
     html_message = f'🌟New V2ray Server!:\n\nType: {server_link.split("://")[0]}\n\n<b>Cick To Copy</b>\n\n<code>{server_link}</code>\n\n🛡 Anti-Censorship Project By @Secuner'
@@ -55,22 +50,56 @@ def send_to_telegram(server_link:str):
         "text": html_message,
         "parse_mode": "HTML"
     }
-    response = requests.post(post_url, data=payload)
-    print(response.text)
+    res = requests.post(post_url, data=payload)
+    print(res.text)
     # print(server_link)
 
-conn = sqlite3.connect('v2ray.db')
-url = 'https://raw.githubusercontent.com/tbbatbb/Proxy/master/dist/v2ray.config.txt'
-response = requests.get(url).text
-servers = base64_to_string(response)
-v2ray_results = server_list_generator(servers)
-for link in v2ray_results:
-    if check_exist_in_db(link) == True:
-        continue
-    elif check_exist_in_db(link) == False:
-        insert_to_db(link)
-        send_to_telegram(link)
-        sleep(5)
 
-conn.commit()
-conn.close()
+def find_config(url, decode: bool = False):
+    conn = sqlite3.connect('v2ray.db')
+    response = requests.get(url).text
+
+    if decode:
+        servers = base64_to_string(response)
+    else:
+        servers = response
+
+    v2ray_results = server_list_generator(servers)
+
+    for link in v2ray_results:
+        if check_exist_in_db(link) is True:
+            continue
+        elif check_exist_in_db(link) is False:
+            insert_to_db(link)
+            send_to_telegram(link)
+            sleep(5)
+
+    conn.commit()
+    conn.close()
+
+
+def main():
+    config_links_decode = [
+        "https://raw.githubusercontent.com/tbbatbb/Proxy/master/dist/v2ray.config.txt",
+        "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity",
+        "https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge_base64.txt",
+        "https://free.iam7.tk/vmess/sub",
+        "https://proxy.yugogo.xyz/vmess/sub",
+        "https://proxy.yiun.xyz/vmess/sub",
+        "https://clash.myvm.cc/vmess/sub",
+        "https://free.jingfu.cf/vmess/sub"
+    ]
+
+    config_links = [
+        "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/splitted/vmess.txt",
+    ]
+
+    for link in config_links_decode:
+        find_config(link, True)
+
+    for link in config_links:
+        find_config(link)
+
+
+if __name__ == "__main__":
+    main()
